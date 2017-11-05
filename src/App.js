5439b0cc2c42a28523c4ef6d9761995e149e9614
Jesse <31576321+agentjesse@users.noreply.github.com>
2017-11-05 01:48:43 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import marked from 'marked';
+// import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
+import ReactHtmlParser from 'react-html-parser';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    //local variables
+    this.poop = marked('I *am* ~~using~~ __markdown__.'); //returns an html encoded string
+    //set the state
+    // this.state = {
+    // };
+  }
+
+  // componentDidMount(){
+  //   console.log(  );
+  // }
+
+  render() {
+    return (
+      <div className="App">
+        
+        { ReactHtmlParser(this.poop) }
+
+      </div>
+    );
+  }
+}
+
+export default App;
